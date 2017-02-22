1c39ab7c1b44c2df76837742794db0b84b5e3114
FernandaEstay <festay12@gmail.com>
2017-02-22 04:43:52 -0300

    zoom con errores

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -8,6 +8,7 @@ public class Raycast : MonoBehaviour {
     private float posX;
     private float posY;
     private Vector3 posInicial;
+    private Vector3 posInicialCam;
 
     // Use this for initialization
     void Start () {
@@ -17,14 +18,22 @@ public class Raycast : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-       
+        if (Input.GetMouseButtonDown(1))
+        {
+            
+            Zoom();
+        }  
     }
-    
+    private void Awake()
+    {
+        posInicialCam = Camera.main.transform.position;
+    }
     private void OnMouseDown()
     {
         dist = Camera.main.WorldToScreenPoint(transform.position);
         posX = Input.mousePosition.x - dist.x;
         posY = Input.mousePosition.y - dist.y;
+        Camera.main.transform.position = posInicialCam;
 
     }
     private void OnMouseUp()
@@ -32,6 +41,7 @@ public class Raycast : MonoBehaviour {
         transform.position = posInicial;
         Vector3 scalePhoto = new Vector3(10, 10, 1);
         transform.localScale = scalePhoto;
+        Camera.main.transform.position = posInicialCam;
     }
 
     private void OnMouseDrag()
@@ -41,10 +51,19 @@ public class Raycast : MonoBehaviour {
         transform.position = worldPos;
         Vector3 scalePhoto = new Vector3(5, 5, 1);
         transform.localScale = scalePhoto;
+        Camera.main.transform.position = posInicialCam;
     }
     private void Zoom()
     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.cyan);
+        RaycastHit hitExit;
 
+        if (Physics.Raycast(ray, out hitExit) == true)
+        {
+            Renderer rend = hitExit.transform.GetComponent<Renderer>();
+            Camera.main.transform.position = new Vector3(hitExit.transform.position.x, hitExit.transform.position.y, -10f);
+        }
     }
 }
 
