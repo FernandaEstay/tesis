e502652825e2edf87205af78f97c7a744ab3ff28
FernandaEstay <festay12@gmail.com>
2017-03-24 12:36:10 -0300

    ZoomIn y ZoomOut gestures

--- Assets/Scripts/ScrFaceKinect.cs
+++ Assets/Scripts/ScrFaceKinect.cs
@@ -9,7 +9,7 @@ using System;
 
 public class ScrFaceKinect : MonoBehaviour
 {
-
+    private float time = 450;
     private KinectSensor kinectSensor;
     private int bodyCount;
     private Body[] bodies;
@@ -17,20 +17,22 @@ public class ScrFaceKinect : MonoBehaviour
     private FaceFrameReader[] faceFrameReaders;
     private BodySourceManager _BodyManager;
     private int updateFrame;
+    private static GameObject photoSelected = null;
 
     private const double FaceRotationIncrementInDegrees = 1.0f;
     private float centerX = 45.5f;
     private float centerY = 31f;
-
     private float multX = -1.42f;
     private float multY = 2.6f;
 
     public GameObject BodySrcManager;
     private BodySourceManager bodyManager;
-    //private LineRenderer lr;
 
-    public GameObject marcador;
+    private Vector3 posAnterior;
+    private static GameObject marcador;
 
+    private Vector3 posRay;
+    private Vector3 posWorld;
 
     void Start()
     {
@@ -83,12 +85,17 @@ public class ScrFaceKinect : MonoBehaviour
             faceFrameReaders[i] = faceFrameSources[i].OpenReader();
         }
 
+        marcador = GameObject.FindGameObjectWithTag("sphere");
         marcador.transform.GetComponent<Renderer>().material.color = UnityEngine.Color.red;
 
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
+        if (ScrZoomKinect.GetZoomStart())
+        {
+            return;
+        }
         if (updateFrame < 1)
         {
             updateFrame++;
@@ -123,66 +130,53 @@ public class ScrFaceKinect : MonoBehaviour
                         var result = frame.FaceFrameResult;
 
                         // extract face rotation in degrees as Euler angles
-                        if (result.FaceRotationQuaternion != null )
+                        if (result.FaceRotationQuaternion != null)
                         {
                             int pitch, yaw, roll;
                             ExtractFaceRotationInDegrees(result.FaceRotationQuaternion, out pitch, out yaw, out roll);
-                            multY = 1.73f;
-                            /*
-                            if (pitch > 8)
-                            {
-                                multY = 1.73f;
-                            }
-                            else if (pitch > 3 && pitch < 8)
-                            {
-                                multY = 0f;
-                            }
-                            else
-                            {
-                                multY = 5.5f;
-                            }*/
-                            Vector3 posRay = new Vector3(yaw * multX + centerX, pitch* multY + centerY, 0);
-                            Vector3 pos = Camera.main.WorldToScreenPoint(posRay);
-                            Ray ray = Camera.main.ScreenPointToRay(pos);
+
+                            posRay = new Vector3(yaw * multX + centerX, pitch * multY + centerY, 0);
+
+
+                            posWorld = Camera.main.WorldToScreenPoint(posRay);
+                            Ray ray = Camera.main.ScreenPointToRay(posWorld);
 
                             RaycastHit hit;
-
-                            Debug.DrawLine(ray.origin, ray.direction, UnityEngine.Color.red);
-                            Debug.DrawLine(ray.origin, posRay, UnityEngine.Color.cyan);
-
-                            marcador.transform.position = posRay;
-
-                            Debug.Log("posRay= " + posRay + "ray= " + ray.origin + 100 * ray.direction);
-
-
+                            
                             if (Physics.Raycast(ray, out hit) == true)
                             {
-                                Debug.Log("choque con photo " + hit.transform.gameObject.tag);
-                                hit.transform.GetComponent<Renderer>().material.color = UnityEngine.Color.yellow;
-                            }
-                            else{
-                                GameObject[] photos = GameObject.FindGameObjectsWithTag("photo");
-                                for (int y = 0; y < photos.Length; y++)
+                                marcador.transform.GetComponent<Renderer>().material.color = UnityEngine.Color.red;
+
+                                if (marcador.transform.position == null)
                                 {
-                                    Renderer rendererPhoto = photos[y].transform.GetComponent<Renderer>();
-                                    if (rendererPhoto.material.color == UnityEngine.Color.yellow)
+                                    marcador.transform.position = hit.transform.position;
+                                }
+                                if (marcador.transform.position != hit.transform.position)
+                                {
+                                    marcador.transform.position = Vector3.MoveTowards(marcador.transform.position, hit.transform.position, Time.deltaTime * time);
+                                    posAnterior = hit.transform.position;
+                                    //marcador.transform.position = hit.transform.position;
+                                    //StartCoroutine(Translade(marcador, hit.transform.position));
+                                }
+                                if (photoSelected == null)
+                                {
+                                    photoSelected = hit.transform.gameObject;
+                                    photoSelected.transform.GetComponent<Renderer>().material.color = UnityEngine.Color.yellow;
+                                }
+                                else
+                                {
+                                    if (photoSelected.transform.name != hit.transform.name)
                                     {
-                                        rendererPhoto.material.color = UnityEngine.Color.white;
+                                        hit.transform.GetComponent<Renderer>().material.color = UnityEngine.Color.yellow;
+                                        photoSelected.transform.GetComponent<Renderer>().material.color = UnityEngine.Color.white;
+                                        photoSelected = hit.transform.gameObject;
                                     }
                                 }
                             }
-                            /*
-                            Vector3 fwd = transform.TransformDirection(posRay);
-
-                            if (Physics.Raycast(transform.position, fwd, 100))
-                            {
-                                Debug.Log("choque");
-                            }
-                            */
-                                //updateFrame = !updateFrame;
                         }
                     }
                 }
+
             }
             else
             {
@@ -195,6 +189,9 @@ public class ScrFaceKinect : MonoBehaviour
             }
         }
     }
+
+
+
     private static void ExtractFaceRotationInDegrees(Windows.Kinect.Vector4 rotQuaternion, out int pitch, out int yaw, out int roll)
     {
         double x = rotQuaternion.X;
@@ -214,202 +211,14 @@ public class ScrFaceKinect : MonoBehaviour
         yaw = (int)(Math.Floor((yawD + ((increment / 2.0) * (yawD > 0 ? 1.0 : -1.0))) / increment) * increment);
         roll = (int)(Math.Floor((rollD + ((increment / 2.0) * (rollD > 0 ? 1.0 : -1.0))) / increment) * increment);
     }
-}
 
-    /*
-    void Update()
+    public static GameObject GetPhotoSelected()
     {
-
-        if (bodyManager == null)
-        {
-            return;
-        }
-        bodies = bodyManager.GetData();
-
-        if (bodies == null)
-        {
-            return;
-        }
-        int i = 0;
-        foreach (var body in bodies)
-        {
-            if (body == null)
-            {
-                continue;
-            }
-            if (body.IsTracked)
-            {
-                Debug.Log("detecta un cuerpo");
-                faceFrameSources[i].TrackingId = body.TrackingId;
-
-                if (faceFrameSources[i].IsTrackingIdValid)
-                {
-                    Debug.Log("Entra a faceFrameSources isTrackingIdValid");
-                    using (FaceFrame frame = faceFrameReaders[i].AcquireLatestFrame())
-                    {
-                        if (frame != null)
-                        {
-                            if (frame.TrackingId == 0)
-                            {
-                                continue;
-                            }
-                        }
-
-                        Debug.Log(" captura resultados");
-
-                        var result = frame.FaceFrameResult;
-
-                        var eyeLeft = result.FacePointsInColorSpace[FacePointType.EyeLeft];
-                        var eyeRigh = result.FacePointsInColorSpace[FacePointType.EyeRight];
-
-                        goEyeLeft.transform.position = new Vector3(eyeLeft.X * 10f, eyeLeft.Y * 10f);
-                        goEyeRight.transform.position = new Vector3(eyeRigh.X * 10f, eyeRigh.Y * 10f);
-
-                        /*     
-                        archivoX.Write(result.FaceRotationQuaternion.X + "\n");
-                        archivoY.Write(result.FaceRotationQuaternion.Y + "\n");
-                        archivoZ.Write(result.FaceRotationQuaternion.Z + "\n");*/
-
-                        /*
-                        Debug.Log("Angulos: " + result.FaceRotationQuaternion.X.ToString() + "," + result.FaceRotationQuaternion.Y.ToString() + "," + result.FaceRotationQuaternion.Z.ToString());
-                        //Pitch Angle neutral = 0 // - 90 = looking down towards the floor  //+90 = looking up towards the ceiling
-                        if (result.FaceRotationQuaternion.X > 90)
-                        {
-                            Debug.Log("Usuario Mira hacia el techo");
-                        }
-                        else if (result.FaceRotationQuaternion.X < -90)
-                        {
-                            Debug.Log("Usuario Mira hacia el suelo");
-                        }
-                        else
-                        {
-                            Debug.Log("Usuario Mira al frente eje X");
-                        }
-
-                        if (result.FaceRotationQuaternion.Y < -90)
-                        {
-                            Debug.Log("Usuario Mira hacia la izquierda");
-                        }
-                        else if (result.FaceRotationQuaternion.Y > 90)
-                        {
-                            Debug.Log("Usuario Mira hacia la derecha");
-                        }
-                        else
-                        {
-                            Debug.Log("Usuario Mira al frente eje Y");
-                        }
-
-                        if (result.FaceRotationQuaternion.Z > -90)
-                        {
-                            Debug.Log("Usuario esta sobre su hombro derecho");
-                        }
-                        else if (result.FaceRotationQuaternion.Z < 90)
-                        {
-                            Debug.Log("Usuario esta sobre su hombro izquierdo");
-                        }
-                        else
-                        {
-                            Debug.Log("Usuario Mira al frente eje Z");
-                        }
-
-                    }
-                }
-                i++;
-            }
-        }
+        return photoSelected;
+    }
+    public static GameObject GetMarcador()
+    {
+        return marcador;
     }
 }
-/*
-        // iterate through each body and update face source
-        for (int i = 0; i < bodyCount; i++)
-        {
-
-            //Compruebe si un rostro válido se rastrea en esta fuente de rostro
-            if (faceFrameSources[i].IsTrackingIdValid)
-            {
-                using (FaceFrame frame = faceFrameReaders[i].AcquireLatestFrame())
-                {
-                    if (frame != null)
-                    {
-                        if (frame.TrackingId == 0)
-                        {
-                            continue;
-                        }
-
-                        // do something with result
-                        
-                        var result = frame.FaceFrameResult;
-
-                        var eyeLeft = result.FacePointsInColorSpace[FacePointType.EyeLeft];
-                        var eyeRigh = result.FacePointsInColorSpace[FacePointType.EyeRight];
-
-                        goEyeLeft.transform.position = new Vector3(eyeLeft.X * 10f, eyeLeft.Y * 10f);
-                        goEyeRight.transform.position = new Vector3(eyeRigh.X * 10f, eyeRigh.Y * 10f);
-
-                        /*     
-                        archivoX.Write(result.FaceRotationQuaternion.X + "\n");
-                        archivoY.Write(result.FaceRotationQuaternion.Y + "\n");
-                        archivoZ.Write(result.FaceRotationQuaternion.Z + "\n");*/
-                        // X - Pitch
-                        // Y - Yaw
-                        // Z - Roll
-
-
-                        //Pitch Angle neutral = 0 // - 90 = looking down towards the floor  //+90 = looking up towards the ceiling
-  /*                      if (result.FaceRotationQuaternion.X > 90)
-                        {
-                            Debug.Log("Usuario Mira hacia el techo");
-                        }
-                        else if(result.FaceRotationQuaternion.X < -90)
-                        {
-                            Debug.Log("Usuario Mira hacia el suelo");
-                        }
-                        else
-                        {
-                            Debug.Log("Usuario Mira al frente eje X");
-                        }
-
-                        if(result.FaceRotationQuaternion.Y < -90)
-                        {
-                            Debug.Log("Usuario Mira hacia la izquierda");
-                        }
-                        else if(result.FaceRotationQuaternion.Y > 90)
-                        {
-                            Debug.Log("Usuario Mira hacia la derecha");
-                        }
-                        else
-                        {
-                            Debug.Log("Usuario Mira al frente eje Y");
-                        }
-
-                        if(result.FaceRotationQuaternion.Z > -90)
-                        {
-                            Debug.Log("Usuario esta sobre su hombro derecho");
-                        }
-                        else if(result.FaceRotationQuaternion.Z < 90)
-                        {
-                            Debug.Log("Usuario esta sobre su hombro izquierdo");
-                        }
-                        else
-                        {
-                            Debug.Log("Usuario Mira al frente eje Z");
-                        }
-
-                    }
-                }
-            }
-
-            else
-            {
-                // check if the corresponding body is tracked 
-                Debug.Log(bodies[i].IsTracked);
-                /*if (bodies[i].IsTraking)
-                {
-                    // update the face frame source to track this body
-                    faceFrameSources[i].TrackingId = bodies[i].TrackingId;
-                }*/
-          /*  }
-        }*/
-
-    
 
---
+++ Assets/Scripts/ScrZoomKinect.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+
+public class ScrZoomKinect : MonoBehaviour {
+
+    private KinectSensor kinectSensor;
+    private Body[] bodies;
+    public GameObject BodySrcManager;
+    private BodySourceManager bodyManager;
+
+    private GameObject photo;
+    private GameObject marcador;
+
+    private bool zoomIn = true;
+    private bool zoomOut = false;
+    private Vector3 posMax;
+    private Vector3 posInicial;
+
+    private static bool ZoomStart = false;
+
+    // Use this for initialization
+    void Start () {
+
+        if (BodySrcManager == null)
+        {
+            Debug.Log("Falta asignar Game Object as BodySrcManager");
+        }
+        else
+        {
+            bodyManager = BodySrcManager.GetComponent<BodySourceManager>();
+        }
+
+        posMax = new Vector3(0f, 0f, -45f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(bodyManager == null)
+        {
+            return;
+        }
+        bodies = bodyManager.GetData();
+        if(bodies == null)
+        {
+            return;
+        }
+        foreach( var body in bodies)
+        {
+            if(body == null)
+            {
+                continue;
+            }
+            if (body.IsTracked)
+            {
+                if(ScrFaceKinect.GetPhotoSelected() != null && ScrFaceKinect.GetMarcador() != null)
+                {
+                    photo = ScrFaceKinect.GetPhotoSelected();
+                    marcador = ScrFaceKinect.GetMarcador();
+                    switch (body.HandRightState)
+                    {
+                        case HandState.Open:
+                            if(zoomIn == true)
+                            {
+                                posInicial = photo.transform.position;
+                                ZoomStart = true;
+                                StartCoroutine(ZoomIn(photo, marcador));
+                            }
+                            else
+                            {
+                                return;
+                            }
+                            break;
+                        case HandState.Closed:
+                            if (zoomOut == true)
+                            {
+                                StartCoroutine(ZoomOut(photo, posInicial, marcador));
+                                ZoomStart = false;
+                            }
+                            else
+                            {
+                                return;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                
+            }
+        }
+        
+    }
+
+    IEnumerator ZoomIn(GameObject photo, GameObject marcador)
+    {
+        zoomIn = false;
+        ObjectChangeColor(photo.transform.gameObject, Color.white);
+        marcador.SetActive(false);
+        while (photo.transform.position.z > posMax.z)
+        {
+            Vector3 posCam = Camera.main.transform.position;
+            posCam.Set(posCam.x, posCam.y, posCam.z + 15f);
+            float step = 5f;
+            photo.transform.position = Vector3.MoveTowards(photo.transform.position, posCam, step);
+            yield return null;
+        }
+        PhotosChangeColor(photo.transform.gameObject, Color.grey);
+        zoomOut = true;
+    }
+
+    IEnumerator ZoomOut(GameObject photo, Vector3 posInicial, GameObject marcador)
+    {
+        zoomOut = false;
+        marcador.SetActive(true);
+        while (photo.transform.position.z < posInicial.z)
+        {
+            float step = 5f;
+            photo.transform.position = Vector3.MoveTowards(photo.transform.position, posInicial, step);
+            yield return null;
+        }
+        PhotosChangeColor(photo.transform.gameObject, Color.white);
+        photo = null;
+        zoomIn = true;
+    }
+
+    private void PhotosChangeColor(GameObject except, Color color)
+    {
+        GameObject[] photos = GameObject.FindGameObjectsWithTag("photo");
+
+        foreach (GameObject photo in photos)
+        {
+            if (photo.name != except.name)
+            {
+                ObjectChangeColor(photo, color);
+            }
+        }
+    }
+
+    private void ObjectChangeColor(GameObject obj, Color color)
+    {
+        Renderer rend = obj.transform.GetComponent<Renderer>();
+        rend.material.color = color;
+    }
+    public static bool GetZoomStart()
+    {
+        return ZoomStart;
+    }
+}
